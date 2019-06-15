00106792a809c50c5fe11c41dc3eb58353dc3572
Tamás László <tlaszlods@gmail.com>
2019-06-15 18:29:00 +0200

    list previous games on home page

--- src/view/Home.js
+++ src/view/Home.js
@@ -1,19 +1,51 @@
 import React, { useContext } from 'react';
 import Box from '@material-ui/core/Box';
 import Button from '@material-ui/core/Button';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import Typography from '@material-ui/core/Typography';
+import { format } from 'date-fns';
 
+import GameContext from './GameContext';
 import MenuContext from './MenuContext';
-import { newGame } from './pageIds';
+import { newGame, scores } from './pageIds';
 
 export default function Home() {
+  const { gameState, dispatch } = useContext(GameContext);
   const { setCurrentPage } = useContext(MenuContext);
   const startNewGame = () => setCurrentPage(newGame);
+  const renderScores = () => (
+    <List>
+      {gameState.games.map(game => (
+        <ListItem
+          key={game.id}
+          button
+          onClick={() => {
+            dispatch({ type: 'SET_CURRENT_GAME', id: game.id });
+            setCurrentPage(scores);
+          }}
+        >
+          <ListItemText
+            primary={format(game.date, 'YYYY.MM.DD HH:mm')}
+            secondary={game.players.join(', ')}
+          />
+        </ListItem>
+      ))}
+    </List>
+  );
 
   return (
     <Box my={2}>
       <Button variant="contained" color="primary" onClick={startNewGame}>
         Új Játék
       </Button>
+      <Typography variant="h6">Játszmák</Typography>
+      {gameState.games.length === 0 ? (
+        <Typography variant="body1">Nincs elérhető mentett játszma.</Typography>
+      ) : (
+        renderScores()
+      )}
     </Box>
   );
 }
