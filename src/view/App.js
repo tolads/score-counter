009ed42960f6f6b24ebcf16de2a7f8c53906cc51
Tamás László <tlaszlods@gmail.com>
2019-06-15 09:47:02 +0200

    add .prettierrc config for Prettier

--- src/view/App.js
+++ src/view/App.js
@@ -1,12 +1,12 @@
-import React from "react";
-import AppBar from "@material-ui/core/AppBar";
-import Box from "@material-ui/core/Box";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import Container from "@material-ui/core/Container";
-import Slide from "@material-ui/core/Slide";
-import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
-import useScrollTrigger from "@material-ui/core/useScrollTrigger";
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Box from '@material-ui/core/Box';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Container from '@material-ui/core/Container';
+import Slide from '@material-ui/core/Slide';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import useScrollTrigger from '@material-ui/core/useScrollTrigger';
 
 function HideOnScroll(props) {
   const { children } = props;
@@ -38,9 +38,9 @@ function App() {
               () => `Cras mattis consectetur purus sit amet fermentum.
   Cras justo odio, dapibus ac facilisis in, egestas eget quam.
   Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-  Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`
+  Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`,
             )
-            .join("\n")}
+            .join('\n')}
         </Box>
       </Container>
     </>
